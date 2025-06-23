6e2359d39004ff0ec9b1e0a08db2c96446baa9b1
Nodir Xakimov <nodirsayfullayevich@gmail.com>
2025-06-23 11:53:25 +0500

    H2 database added, model and service updated

--- src/main/java/com/amigoscode/person/PersonService.java
+++ src/main/java/com/amigoscode/person/PersonService.java
@@ -20,21 +20,21 @@ public class PersonService {
 
     public List<Person> getPeople(SortingOrder sort) {
         if (sort == SortingOrder.ASC) {
-            return personRepository.getPeople().stream().sorted(Comparator.comparing(Person::id)).toList();
+            return personRepository.getPeople().stream().sorted(Comparator.comparing(Person::getId)).toList();
         }
-        return personRepository.getPeople().stream().sorted(Comparator.comparing(Person::id).reversed()).toList();
+        return personRepository.getPeople().stream().sorted(Comparator.comparing(Person::getId).reversed()).toList();
     }
 
     public Person getPersonById(Integer id) {
         return personRepository.getPeople().stream()
-                .filter(p -> Objects.equals(p.id(), id))
+                .filter(p -> Objects.equals(p.getId(), id))
                 .findFirst()
                 .orElseThrow(() -> new ResourceNotFoundException("Person with id " + id + " does not exist."));
     }
 
     public void deletePersonById(Integer id) {
         Person person = personRepository.getPeople().stream()
-                .filter(p -> Objects.equals(p.id(), id))
+                .filter(p -> Objects.equals(p.getId(), id))
                 .findFirst()
                 .orElseThrow(() -> new ResourceNotFoundException("Person with id " + id + " does not exist."));
         personRepository.getPeople().remove(person);
@@ -43,7 +43,7 @@ public class PersonService {
     public void addPerson(NewPersonRequest person) {
         if(person.email() != null && !person.email().isEmpty()) {
             boolean exist = personRepository.getPeople().stream()
-                    .anyMatch(p -> p.email().equalsIgnoreCase(person.email()));
+                    .anyMatch(p -> p.getEmail().equalsIgnoreCase(person.email()));
             if(exist) {
                 throw new DuplicateResourceException("Email taken");
             }
@@ -60,27 +60,27 @@ public class PersonService {
     public void updatePerson(Integer id, PersonUpdateRequest request) {
         Person p = personRepository.getPeople()
                 .stream()
-                .filter(person -> Objects.equals(person.id(), id))
+                .filter(person -> Objects.equals(person.getId(), id))
                 .findFirst()
                 .orElseThrow(() -> new ResourceNotFoundException("Person with id " + id + " does not exist."));
 
         var index = personRepository.getPeople().indexOf(p);
-        if (request.name() != null && !request.name().isEmpty() && !request.name().equals(p.name())) {
+        if (request.name() != null && !request.name().isEmpty() && !request.name().equals(p.getName())) {
             personRepository.getPeople().set(index, new Person(
-                    p.id(),
+                    p.getId(),
                     request.name(),
-                    p.age(),
-                    p.gender(),
-                    p.email()
+                    p.getAge(),
+                    p.getGender(),
+                    p.getEmail()
             ));
         }
-        if (request.age() != null && !request.age().equals(p.age())) {
+        if (request.age() != null && !request.age().equals(p.getAge())) {
             Person person = new Person(
-                    p.id(),
-                    p.name(),
+                    p.getId(),
+                    p.getName(),
                     request.age(),
-                    p.gender(),
-                    p.email()
+                    p.getGender(),
+                    p.getEmail()
             );
             personRepository.getPeople().set(index, person);
         }
